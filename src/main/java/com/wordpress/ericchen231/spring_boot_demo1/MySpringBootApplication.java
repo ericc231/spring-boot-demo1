cf6b51e9baae5610124e9c2a8a97f7b2f7babf0a
ericc231 <62298605+ericc231@users.noreply.github.com>
2020-03-17 23:55:04 +0800

    Initial project

---
+++ src/main/java/com/wordpress/ericchen231/spring_boot_demo1/MySpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.wordpress.ericchen231.spring_boot_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBootApplication {
+
+    /**
+     * A main method to start this application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringBootApplication.class, args);
+    }
+
+}
